039efb795e1d4d745f9c22e8904f0df56252f956
Yuri Lifanov <yuri.li.urta2210@gmail.com>
2022-06-25 17:20:17 +0300

    add function getReport

--- TemperatureMonitoringWinForms/Fish.cs
+++ TemperatureMonitoringWinForms/Fish.cs
@@ -11,7 +11,10 @@ namespace TemperatureMonitoring
         int TimeExceededMaxTemperature;
         int MinStorageTemperature;
         int TimeExceededMinTemperature;
+        bool isMinValue = false;
         int currentViolationMaxTem = 0;
+        int currentViolationMinTem = 0;
+
 
         public Fish(string KindOfFish, int MaxStorageTemperature, int TimeExceededMaxTemperature)
         {
@@ -28,15 +31,38 @@ namespace TemperatureMonitoring
             this.TimeExceededMaxTemperature = TimeExceededMaxTemperature;
             this.MinStorageTemperature = MinStorageTemperature;
             this.TimeExceededMinTemperature = TimeExceededMinTemperature;
+            isMinValue = true;
         }
 
-        public void CreateREport(TemperatureSensorReport tsReport)
+        public string CreateREport(TemperatureSensorReport tsReport)
         {
+            string res = "";
+            string resMin = "";
+            string resMax = "";
             Dictionary<DateTime, int> TemperatureReport = tsReport.GetReport();
             foreach (var i in TemperatureReport)
             {
-                
+                if (i.Value > MaxStorageTemperature)
+                {
+                    currentViolationMaxTem += 10;
+                    resMax += $"{i.Key}\t {i.Value}\t {MaxStorageTemperature}\t {i.Value - MaxStorageTemperature}\n";
+                }
+                else if (isMinValue && i.Value < MinStorageTemperature)
+                {
+                    currentViolationMinTem += 10;
+                    resMin += $"{i.Key}\t {i.Value}\t {MinStorageTemperature}\t {i.Value - MinStorageTemperature}\n";
+                }
             }
+            if (currentViolationMaxTem > TimeExceededMaxTemperature || currentViolationMinTem > TimeExceededMinTemperature)
+            {
+                int time = currentViolationMaxTem > TimeExceededMaxTemperature ? currentViolationMaxTem : currentViolationMinTem;
+                string timeout = time > 60 ?  $"{time / 60} часа {time - (time % 60)}" : $"{time}";
+                string MinMax = currentViolationMaxTem > TimeExceededMaxTemperature ? "максимально" : "минимально";
+                res += $"Порог {MinMax} допустимой температуры превышен на {timeout} минут.\n";
+                res += $"Время\t\t\t Факт\t Норма\t Отклонение\n";
+                res += currentViolationMaxTem > TimeExceededMaxTemperature ? resMax : resMin; 
+            }
+            return res;
         }
     }
 }
